d460c36fed58252797037b7d4c8246ef4a958fca
petros <pit.trak@gmail.com>
2020-06-28 11:31:23 +0300

    added enum with roles

---
+++ SpringBootAuthentication/src/main/java/com/petros/springjwt/models/ERole.java
@@ -0,0 +1,7 @@
+package com.petros.springjwt.models;
+
+public enum ERole {
+	ROLE_USER,
+	ROLE_MODERATOR,
+	ROLE_ADMIN
+}
---
+++ SpringBootAuthentication/src/main/java/com/petros/springjwt/models/Role.java
@@ -0,0 +1,8 @@
+package com.petros.springjwt.models;
+
+import javax.persistance.*;
+
+@Entity
+public class Role {
+
+}
