6b2cd2b32282faca71d77ee34cb0360c85fedb55
petros <pit.trak@gmail.com>
2020-06-29 22:37:59 +0300

    filan fixes in App.js

--- react-jwt-auth/src/App.js
+++ react-jwt-auth/src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
         <div>
           <nav className="navbar navbar-expand navbar-dark bg-dark">
             <Link to={"/"} className="navbar-brand">
-              bezKoder
+              PetrosTrak
             </Link>
             <div className="navbar-nav mr-auto">
               <li className="nav-item">
