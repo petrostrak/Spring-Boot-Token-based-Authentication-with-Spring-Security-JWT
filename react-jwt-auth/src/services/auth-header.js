784c44b194d0a9d7b0a6f9565ed3140d7c3727c6
petros <pit.trak@gmail.com>
2020-06-29 20:52:44 +0300

    Added helper method auth-header to check db for logged in users

---
+++ react-jwt-auth/src/services/auth-header.js
@@ -0,0 +1,17 @@
+/* We also have methods for retrieving data from server. 
+   In the case we access protected resources, the HTTP 
+   request needs Authorization header. Let’s create a 
+   helper function called authHeader() */
+
+export default function authHeader(){
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if(user && user.accessToken){
+        return {Authorization: 'Bearer ' + user.accessToken};
+    }else return {};
+}   
+
+/* The code above checks Local Storage for user item. 
+   If there is a logged in user with accessToken (JWT), 
+   return HTTP Authorization header. Otherwise, return 
+   an empty object. */
