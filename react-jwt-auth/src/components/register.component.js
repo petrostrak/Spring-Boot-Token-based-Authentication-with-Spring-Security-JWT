de8ac77f4448e0ac75f31212be0441c4712965ce
petros <pit.trak@gmail.com>
2020-07-19 20:04:36 +0300

    updated register component

--- react-jwt-auth/src/components/register.component.js
+++ react-jwt-auth/src/components/register.component.js
@@ -125,7 +125,7 @@ export default class Register extends Component {
       <div className="col-md-12">
         <div className="card card-container">
           <img
-            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            src="me.jpg"
             alt="profile-img"
             className="profile-img-card"
           />
